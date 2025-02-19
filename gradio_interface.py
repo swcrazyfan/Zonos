26fd5e9b1987db74f4ef89913259e6602c924f6d
swcrazyfan <79735340+swcrazyfan@users.noreply.github.com>
2025-02-19 11:44:54 +0800

    Update gradio_interface.py

--- gradio_interface.py
+++ gradio_interface.py
@@ -415,5 +415,5 @@ def build_interface():
 
 if __name__ == "__main__":
     demo = build_interface()
-    share = getenv("GRADIO_SHARE", "False").lower() in ("true", "1", "t")
+    share = getenv("GRADIO_SHARE", "True").lower() in ("true", "1", "t")
     demo.launch(server_name="0.0.0.0", server_port=7860, share=share)
